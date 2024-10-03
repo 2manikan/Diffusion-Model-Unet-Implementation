db0ca49dbb2474f3c642b72994e7577cfe2510aa
Meenakshi Manikandan <2mmanikan@gmail.com>
2024-10-02 21:40:45 -0700

    updated unet code and added generation sequence

--- unet.py
+++ unet.py
@@ -20,13 +20,13 @@ def ts_embed(timesteps, dim):
     half_dim=dim//2
     exponent=-math.log(10000) * torch.arange(start=0, end=half_dim, dtype=torch.float32).to("cuda")
     exponent=exponent/(half_dim - 0)
-    emb=torch.exp(exponent)
+    emb=torch.exp(exponent) #because exp^log(x)=x
     emb=timesteps[:, None].float() * emb[None, :]
     emb=emb*1
     emb=torch.cat([torch.sin(emb), torch.cos(emb)], dim=-1)
     
 
-    #padding with zeros
+    #padding with zeros if needed
     if dim%2==1:
        emb=torch.nn.functional.pad(emb, (0,1,0,0))
 
@@ -39,8 +39,7 @@ class RBlock(nn.Module):
     self.first_norm=nn.GroupNorm(num_groups=num_groups, num_channels=in_channels)
     self.nonlinear=nn.SiLU()
 
-    if down:
-      self.conv_block=nn.AvgPool2d(kernel_size=2,stride=2)
+    
     self.down=down
     self.up=up
 
@@ -50,7 +49,7 @@ class RBlock(nn.Module):
     self.time_layer=nn.Linear(time_dim, out_channels)
     self.second_norm=nn.GroupNorm(num_groups=num_groups, num_channels=out_channels)
 
-    self.use_shortcut=in_channels != out_channels #enough for this impl.
+    self.use_shortcut=in_channels != out_channels #enough for this impl. It's used to change the number of channels of original image
     self.channel_change=None
     if self.use_shortcut==True:
        self.channel_change=nn.Conv2d(in_channels,out_channels,kernel_size=1,stride=1,padding=0,bias=True) 
@@ -67,7 +66,6 @@ class RBlock(nn.Module):
     feature_map=self.nonlinear(feature_map)
     feature_map=self.second_conv_layer(feature_map)
 
-    #added rn by me!!!!!!!!!!!!!
     if self.channel_change!=None:
        image=self.channel_change(image)
 
@@ -78,27 +76,31 @@ class UnetModel(nn.Module):
   def __init__(self):
     super().__init__()
     num_groups=4
-    t_dim=16*4
+    down_layer_number=up_layer_number=6
+    res_per_down=2
+    res_per_up=3
+    self.channel_numbers=[16, 32, 64, 128, 256, 512] #can change this
+    t_dim=self.channel_numbers[0]*4
 
     #timestep stuff
     self.ts_layers=nn.Sequential(
-        nn.Linear(16,16*4,True), #bc 16 is the "out_layer" as shown below. sample_proj_bias is true.
+        nn.Linear(self.channel_numbers[0],t_dim,True), #bc 16 is the "out_layer" as shown below. sample_proj_bias is true.
         nn.SiLU(),
-        nn.Linear(16*4,16*4,True)
+        nn.Linear(t_dim,t_dim,True)
     )
 
-    self.start=nn.Conv2d(3,16,kernel_size=3, stride=1, padding=1)
+    self.start=nn.Conv2d(3,self.channel_numbers[0],kernel_size=3, stride=1, padding=1)
 
     self.down_blocks=nn.ModuleList([])
     
-    self.channel_numbers=[16, 32, 64, 128, 256, 512] #can change this
+    
     out_channels=self.channel_numbers[0]
   
-    for i in range(0,6): #down_layers
+    for i in range(0,down_layer_number): #down_layers
       in_channels=out_channels
       out_channels=self.channel_numbers[i]
       final_block = i ==len(self.channel_numbers)-1
-      for j in range(2): #each layer
+      for j in range(res_per_down): #each layer within one down layer
         in_channels2=in_channels if j==0 else out_channels
         self.down_blocks.append(
             RBlock(in_channels2,out_channels,False,False,t_dim,num_groups)
@@ -106,7 +108,6 @@ class UnetModel(nn.Module):
         
       if not final_block:
         self.down_blocks.append(
-          #nn.AvgPool2d(kernel_size=2,stride=2) #if conv=False
 	  nn.Conv2d(out_channels,out_channels,kernel_size=3,stride=2,padding=1,bias=True) #as conv=True
         )
 
@@ -121,7 +122,7 @@ class UnetModel(nn.Module):
     reverse_channels=list(reversed(self.channel_numbers))
     out_channels=reverse_channels[0]
     self.up_blocks=nn.ModuleList([])
-    for i in range(0,6):
+    for i in range(0,up_layer_number):
       prev_out_channels=out_channels
       out_channels=reverse_channels[i]
       in_channels=reverse_channels[min(i+1,len(reverse_channels)-1)]
@@ -129,8 +130,9 @@ class UnetModel(nn.Module):
 
           
       
-      for j in range(3):
-        skip_channels=in_channels if (j==3-1) else out_channels
+      for j in range(res_per_up):
+        #CAN MODIFY SKIP CONNECTION PART OF ARCHITECTURE HERE
+        skip_channels=in_channels if (j==res_per_up-1) else out_channels
         res_in_channels = prev_out_channels if j==0 else out_channels
         
         self.up_blocks.append(
@@ -138,16 +140,17 @@ class UnetModel(nn.Module):
           )
       if not final_block:
          self.up_blocks.append(nn.Conv2d(out_channels,out_channels,kernel_size=3,padding=1,bias=True))
+         #interpolate part of upsample block will be in forward function.
         
       prev_out_channels=out_channels
-      #upsample block will be in forward function.
+      
       
       
 
     self.end_process=nn.Sequential(
         nn.GroupNorm(num_groups=num_groups, num_channels=16, eps=1e-5), #make norm_eps=1e-5 
         nn.SiLU(),
-        nn.Conv2d(16,3,kernel_size=3,padding=1)
+        nn.Conv2d(self.channel_numbers[0],3,kernel_size=3,padding=1)
     )
 
   def forward(self, x, t):
@@ -161,7 +164,6 @@ class UnetModel(nn.Module):
 
     
     #downsample
-
     sk_c=[x.view(x.size())]
     for i in range(0,len(self.down_blocks)):
       if i%3!=2:
@@ -172,22 +174,10 @@ class UnetModel(nn.Module):
       sk_c.append(x)
     
     #middle
-    #for i in range(len(self.middle_blocks)):
     x=self.middle_blocks[0](x,t)
     x=self.middle_blocks[1](x,t)
     
     #upsample
-    #count=0
-    #i=0
-    #while(count<len(self.up_blocks)): #+1 bc the block just ends at the last RESNET upsampling. We also want to upsample to get to the correct image size.    
-    #  if i%4!=3:
-    #    x=self.up_blocks[count](torch.cat([x,sk_c.pop()],dim=1), t)
-    #    count+=1
-    #  else:
-    #    #if count!=len(self.up_blocks)-1:
-    #    x=nn.functional.interpolate(x,scale_factor=2,mode='nearest')   
-    #  i+=1
-
     for i in range(0,len(self.up_blocks)):
         if i%4!=3:
            x=self.up_blocks[i](torch.cat([x,sk_c.pop()],dim=1), t)
